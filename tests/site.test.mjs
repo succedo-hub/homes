01568074a02615fdf3e7e2877b81234398ab9504
antonstrandvik <anton.strandvik@alcom.ax>
2026-08-29 11:53:58 +0300

    Update property introduction

--- tests/site.test.mjs
+++ tests/site.test.mjs
@@ -14,7 +14,7 @@ test('alla centrala sidor är byggda', async () => {
 
 test('objektsidan innehåller verifierade kärnfakta och ansvarsfriskrivning', async () => {
   const html = await read('/bostader/4-rum-och-kok-i-parhus-i-mariehamn/index.html');
-  for (const text of ['94,1 m²', '1 290 €', 'Bostads Ab Svärtan', '1 januari 2027', 'tillgänglig för dialog och visning nu', 'uthyres omöblerad från 1.1.2027', 'Våningsplan 2', 'uteplatsen ligger i västerläge', 'annonsplattform']) assert.match(html, new RegExp(text, 'i'));
+  for (const text of ['94,1 m²', '1 290 €', 'Bostads Ab Svärtan', '1 januari 2027', 'tre separata sovrum', 'gemensam i bolaget som består av två lägenheter', 'tillgänglig för dialog och visning nu', 'uthyres omöblerad från 1.1.2027', 'Vissa möbler kan ingå i hyran enligt överenskommelse', 'Våningsplan 2', 'uteplatsen ligger i västerläge', 'mindre odlingar och samvaro', 'annonsplattform']) assert.match(html, new RegExp(text, 'i'));
   assert.match(html, /application\/ld\+json/);
   assert.match(html, /action="https:\/\/formspree.io\/f\/xbgjrepy"/);
 });
